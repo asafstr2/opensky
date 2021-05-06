ff5a342134e33f9f278d4b20913f98bf9470c02f
meniAvitan <66748984+meniAvitan@users.noreply.github.com>
2021-05-06 14:22:34 +0300

    l

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import { mailgun } from './utils.js';
+import path from 'path';
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("gooooooood");
+// app.get("/", (req, res) => {
+//   res.send("gooooooood");
+// });
+//app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res)=>{
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
 });
 
 app.post('/api/form', (req, res) => {
