5756c4e6b9e71cc5fdae525b0a64de0975a3c65a
meniAvitan <66748984+meniAvitan@users.noreply.github.com>
2021-05-06 14:38:56 +0300

    push

--- server.js
+++ server.js
@@ -12,9 +12,8 @@ app.use(cors());
 //   res.send("gooooooood");
 // });
 const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'))
 );
 
 app.post('/api/form', (req, res) => {
