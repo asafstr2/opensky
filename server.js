1cc44c7b7445ea12b79a7013a06dca1234e98f80
asafstr2 <asafstr2@gmail.com>
2021-05-08 19:17:23 +0300

    heroku

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.post("/api/form", (req, res) => {
     );
 });
 
-// if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
 // Express will serve up production assets
 // like our main.js file, or main.css file!
 app.use(express.static("frontend/build"));
@@ -51,7 +51,7 @@ app.use(express.static("frontend/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
-// }
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`serve at http://loclahost:${port}`);
